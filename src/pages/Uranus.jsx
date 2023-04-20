3de5dab54a290b24c7a5e904c81f6fb46a858bd1
Todu66 <giga.todua06@gmail.com>
2023-04-20 11:54:56 +0400

    added circle images on responsive navbar

--- src/pages/Uranus.jsx
+++ src/pages/Uranus.jsx
@@ -1,15 +1,27 @@
-import '../components/Planet.css'
-import Planet from '../components/Planet'
-import uranus1 from '../images/uranus-1.png'
-import uranus2 from '../images/uranus-2.png'
-import uranus3 from '../images/uranus-3.png'
-
+import "../components/Planet.css";
+import Planet from "../components/Planet";
+import uranus1 from "../images/uranus-1.png";
+import uranus2 from "../images/uranus-2.png";
+import uranus3 from "../images/uranus-3.png";
+import uranusOval from "../images/uranusOval.png";
 function Uranus() {
   return (
     <>
-      <Planet name="URANUS" info="Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus according to Greek mythology, was the great-grandfather of Ares. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System." image1={uranus1} image2={uranus2} image3={uranus3} time="17.2 HOURS" revolution="84 YEARS" radius="25,362 KM" temp="-195°C" color="#1EC1A2" />
+      <Planet
+        name="URANUS"
+        info="Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus according to Greek mythology, was the great-grandfather of Ares. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System."
+        image1={uranus1}
+        image2={uranus2}
+        image3={uranus3}
+        image4={uranusOval}
+        time="17.2 HOURS"
+        revolution="84 YEARS"
+        radius="25,362 KM"
+        temp="-195°C"
+        color="#1EC1A2"
+      />
     </>
   );
 }
-  
+
 export default Uranus;
