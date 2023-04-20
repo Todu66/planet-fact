1f3e275c9afce47281ace4dad623f1bcc98fb0d9
Todu66 <giga.todua06@gmail.com>
2023-04-20 11:54:50 +0400

    added circle images on responsive navbar

--- src/pages/Saturn.jsx
+++ src/pages/Saturn.jsx
@@ -1,14 +1,25 @@
-import '../components/Planet.css'
-import Planet from '../components/Planet'
-import saturn1 from '../images/saturn-1.png'
-import saturn2 from '../images/saturn-2.png'
-import saturn3 from '../images/saturn-3.png'
-
-
+import "../components/Planet.css";
+import Planet from "../components/Planet";
+import saturn1 from "../images/saturn-1.png";
+import saturn2 from "../images/saturn-2.png";
+import saturn3 from "../images/saturn-3.png";
+import saturnOval from "../images/saturnOval.png";
 function Saturn() {
   return (
     <>
-      <Planet name="SATURN" info="Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth. It only has one-eighth the average density of Earth." image1={saturn1} image2={saturn2} image3={saturn3} time="10.8 HOURS" revolution="29.46 YEARS" radius="58,232 KM" temp="-138°C" color="#CD5120" />
+      <Planet
+        name="SATURN"
+        info="Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth. It only has one-eighth the average density of Earth."
+        image1={saturn1}
+        image2={saturn2}
+        image3={saturn3}
+        image4={saturnOval}
+        time="10.8 HOURS"
+        revolution="29.46 YEARS"
+        radius="58,232 KM"
+        temp="-138°C"
+        color="#CD5120"
+      />
     </>
   );
 }
